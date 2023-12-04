66aaededdd0a284bf0e1f4dad48987fd4ffd3531
RodrigoFranco1 <115948997+RodrigoFranco1@users.noreply.github.com>
2023-12-04 14:43:02 -0600

    Create leaker.py

---
+++ leaker.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+dehashed.py
+
+Realiza una búsqueda en la API de DeHashed.
+
+Autor: Tu Nombre
+Fecha: Fecha de Creación
+Licencia: Tipo de Licencia (si aplica)
+Versión: Número de versión del script
+
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+import argparse
+import time
+
+# Define tu clave API aquí
+API_KEY = 'hpc9v6t26regx5r39qrj3s8qieflpcsr'
+
+def buscar_en_dehashed(auth, search_field, query, page, size):
+    base_url = "https://api.dehashed.com/search"
+    search_query = f"{search_field}:{query}" if search_field != 'all' else query
+    url = f"{base_url}?query={search_query}&page={page}&size={size}"
+    headers = {'Accept': 'application/json'}
+
+    try:
+        response = requests.get(url, headers=headers, auth=auth)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as err:
+        response_text = err.response.text if err.response else "Sin respuesta"
+        return f"Error de HTTP: {err}\nRespuesta: {response_text}"
+    except Exception as err:
+        return f"Error General: {err}"
+
+def recorrer_todas_las_paginas(auth, search_field, query, size):
+    print(f"Iniciando búsqueda para: {search_field}='{query}'")
+    page = 1
+    registro_numero = 1
+    while True:
+        print(f"Solicitando página {page}")
+        resultado = buscar_en_dehashed(auth, search_field, query, page, size)
+
+        if isinstance(resultado, str):
+            print(resultado)
+            break
+
+        if not resultado.get('entries'):
+            print("No se encontraron más resultados o saldo agotado.")
+            break
+
+        for entrada in resultado['entries']:
+            print(f"Registro {registro_numero}:")
+            for key, value in entrada.items():
+                print(f"  {key.capitalize().replace('_', ' ')}: {value}")
+            print("\n")
+            registro_numero += 1
+
+        page += 1
+        time.sleep(0.2)
+
+def procesar_lista(auth, search_field, file_path, size):
+    with open(file_path, 'r') as file:
+        for line in file:
+            target = line.strip()
+            if target:
+                search_query = target if search_field == 'all' else f"{search_field}:{target}"
+                print(f"Buscando {search_field} = {target}")
+                recorrer_todas_las_paginas(auth, search_field, search_query, size)
+
+epilog_text = (
+    "Ejemplos de uso:\n\n"
+    "  Buscar por email:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search email -t \"usuario@ejemplo.com\"\n\n"
+    "  Buscar por nombre de usuario:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search username -t \"nombreusuario\"\n\n"
+    "  Buscar por dirección IP:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search ip_address -t \"192.168.1.1\"\n\n"
+    "  Buscar por dominio:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search domain -t \"ejemplo.com\"\n\n"
+    "  Buscar por nombre:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search name -t \"John Doe\"\n\n"
+    "  Buscar en todos los campos:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search all -t \"termino_de_busqueda\"\n\n"
+    "  Buscar utilizando una lista de términos:\n"
+    "    python dehashed.py --email tu_correo@ejemplo.com --search all -l \"lista.txt\"\n"
+)
+
+def mostrar_logo():
+    logo = """
+ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠃⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘
+⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀
+⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸
+⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇
+⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃
+⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀
+⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀
+⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+linkedin: @rodrigofrancob
+             
+    """
+    print(logo)
+
+def main():
+    mostrar_logo()
+
+    parser = argparse.ArgumentParser(
+        description='Realiza una búsqueda usando la API de DeHashed.',
+        epilog=epilog_text,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument('--email', required=True, help='Tu correo electrónico registrado en DeHashed.')
+    parser.add_argument('--search', help='El campo de búsqueda ...')
+    parser.add_argument('-t', '--target', help='El valor objetivo para la búsqueda.')
+    parser.add_argument('-l', '--list', help='Ruta a un archivo con una lista de objetivos para la búsqueda.')
+
+    args = parser.parse_args()
+
+    auth = HTTPBasicAuth(args.email, API_KEY)
+
+    if args.list:
+        procesar_lista(auth, args.search, args.list, 100)
+    elif args.target:
+        recorrer_todas_las_paginas(auth, args.search, args.target, 100)
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
